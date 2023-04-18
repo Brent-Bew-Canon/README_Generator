93f260eec01ba6f23b6c0bae21aa8677ccd52a7e
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-04-18 11:26:18 -0700

    Input questions + prompt sequence

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+// npm i inquirer@8.2.4
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your github name?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'How will you describe this application?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for this application?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What usage information is needed for this application?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contributions guidelines for this application?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for this application?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What type of license does this application have?',
+        name: 'test',
+    }
+];
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt(questions)
+    .then((response) =>
+        console.log(response)
+    );
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName + ".md", JSON.stringify(response), err => {
+        if (err) {
+            console.error("There's been an error: " + err)
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
